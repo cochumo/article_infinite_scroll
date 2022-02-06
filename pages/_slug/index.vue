e72413a595f1d2bc7da066c535d7a31c19b77331
cochumo <stream99990@gmail.com>
2022-02-07 02:58:00 +0900

    remove: いらない部分を削除しました

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -12,7 +12,6 @@ export default Vue.extend({
   name: 'SlugPage',
   async asyncData({ $microcms, _params }: Context & any): Promise<any> {
     const post = await $microcms.get({
-      // endpoint: `post/${_params.slug}`,
       endpoint: `post/bqwj4f59ec`,
     })
     return {
