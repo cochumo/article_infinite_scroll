7791f08b6bda86d669223be38b1b14781787f456
cochumo <stream99990@gmail.com>
2022-02-07 02:50:21 +0900

    update: 記事をmicroCmsから取得しました

---
+++ pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-html="post.contents" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Context } from '@nuxt/types';
+
+export default Vue.extend({
+  name: 'SlugPage',
+  async asyncData({ $microcms, _params }: Context & any): Promise<any> {
+    const post = await $microcms.get({
+      // endpoint: `post/${_params.slug}`,
+      endpoint: `post/bqwj4f59ec`,
+    })
+    return {
+      post,
+    }
+  }
+})
+</script>
